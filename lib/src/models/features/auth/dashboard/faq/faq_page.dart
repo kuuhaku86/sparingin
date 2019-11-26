efbac89e887077a1f5f84ca16adffdeb057a34a8
Yohan Ardiansyah <yohan.ardiansyah90@gmail.com>
2019-11-26 14:05:06 +0700

    Finishing faq page

--- lib/src/models/features/auth/dashboard/faq/faq_page.dart
+++ lib/src/models/features/auth/dashboard/faq/faq_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:sparingin/src/utils/app_bar.dart';
 import 'package:sparingin/src/utils/bottom_bar.dart';
+import 'package:sparingin/src/utils/build_padding.dart';
+import 'package:sparingin/src/utils/colors.dart';
 
 class FaqPage extends StatefulWidget {
   FaqPage({Key key}) : super(key: key);
@@ -14,8 +16,29 @@ class _FaqPageState extends State<FaqPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildAppBar("FAQ", null),
-      body: ListView(
-        
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Center(
+            child: Text(
+              "E-mail : cp@gmail.com",
+              style: TextStyle(
+                fontSize: 25,
+                color: MyColors.font,
+              ),
+            ),
+          ),
+          buildPadding(30),
+          Center(
+            child: Text(
+              "Phone : 081234567890",
+              style: TextStyle(
+                fontSize: 25,
+                color: MyColors.font,
+              ),
+            ),
+          ),
+        ],
       ),
       bottomNavigationBar: DefaultBottomBar(),
     );
