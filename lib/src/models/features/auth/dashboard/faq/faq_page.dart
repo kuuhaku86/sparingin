98059cee573f6356e6a76f0d1e5281b2bb422c0e
Yohan Ardiansyah <yohan.ardiansyah90@gmail.com>
2019-11-26 13:18:55 +0700

    Finishing tim kami page

--- lib/src/models/features/auth/dashboard/faq/faq_page.dart
+++ lib/src/models/features/auth/dashboard/faq/faq_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:sparingin/src/utils/app_bar.dart';
+import 'package:sparingin/src/utils/bottom_bar.dart';
 
 class FaqPage extends StatefulWidget {
   FaqPage({Key key}) : super(key: key);
@@ -10,8 +12,12 @@ class FaqPage extends StatefulWidget {
 class _FaqPageState extends State<FaqPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-       child: Scaffold(),
+    return Scaffold(
+      appBar: buildAppBar("FAQ", null),
+      body: ListView(
+        
+      ),
+      bottomNavigationBar: DefaultBottomBar(),
     );
   }
 }
--- lib/src/models/features/auth/dashboard/timKami/tim_kami_page.dart
+++ lib/src/models/features/auth/dashboard/timKami/tim_kami_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:sparingin/src/utils/app_bar.dart';
+import 'package:sparingin/src/utils/bottom_bar.dart';
+import 'package:sparingin/src/utils/build_padding.dart';
+import 'package:sparingin/src/utils/colors.dart';
 
 class TimKamiPage extends StatefulWidget {
   TimKamiPage({Key key}) : super(key: key);
@@ -10,8 +14,49 @@ class TimKamiPage extends StatefulWidget {
 class _TimKamiPageState extends State<TimKamiPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-       child: Scaffold(),
+    return Scaffold(
+      appBar: buildAppBar("TIM KAMI", null),
+      body: ListView(
+        children: <Widget>[
+          buildPadding(20),
+          Image.asset(
+            "assets/images/card/juaraFutsal.jpg",
+          ),
+          buildPadding(20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.asset(
+                "assets/images/card/krustykrab.png",
+                height: 80,
+              ),
+              SizedBox(width: 4,),
+              Column(
+                children: <Widget>[
+                  Text(
+                    "Tim Juara",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w700,
+                      fontSize: 25,
+                    ),
+                  ),
+                  buildPadding(3),
+                  Text(
+                    "Gampanglah Buat Juara",
+                    style: TextStyle(
+                      color: MyColors.font,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          )
+        ],
+      ),
+      bottomNavigationBar: DefaultBottomBar(),
     );
   }
 }
